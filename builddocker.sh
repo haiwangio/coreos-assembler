d5f13dbe06c37895bfaa4de177e78cce9f95af62
root <root@localhost.localdomain>
2018-11-08 07:08:06 +0800

    fedora 29 docker

---
+++ builddocker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: builddocker.sh
+#
+#         USAGE: ./builddocker.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2018年11月08日 06时52分44秒
+#      REVISION:  ---
+#===============================================================================
+
+docker build -t haianwangio/coreos-assembler .
